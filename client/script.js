68f43f56f824b670d74b9ea1846987a2ae9ca742
Juan Osorio <osorioj202@gmail.com>
2023-03-23 18:22:38 -0500

    server update

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://mychatgpt-3zuy.onrender.com', {
+    const response = await fetch('https://mychatgpt-3zuy.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
